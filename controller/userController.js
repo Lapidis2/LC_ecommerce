3e8df1ec19db9b761832f8c6972ed12c3e5c0163
HITAYEZU Jean Pierre <hitlapidis2@gmail.com>
2024-09-27 14:25:22 +0200

    new updates code

--- controller/userController.js
+++ controller/userController.js
@@ -11,7 +11,8 @@ const signup_post =async (req, res) => {
     const newUser = {
         rname: name,
         email: email,
-        password: password 
+        password: password,
+        role:'admin'
     }
 
     console.log(newUser); 
