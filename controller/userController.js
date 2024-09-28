d6224ce854c28004ccd43e6e420c3e9a70dc82d5
HITAYEZU Jean Pierre <hitlapidis2@gmail.com>
2024-09-28 22:22:17 +0200

    sign up codes added

--- controller/userController.js
+++ controller/userController.js
@@ -1,11 +1,11 @@
+// @ts-nocheck
 require('dotenv').config();
-const User= require('../modal/userModal');
+const User = require('../modal/userModal');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const nodemailler = require('nodemailer');
 const crypto = require('crypto');
 
-// Controller for handling signup form submission
 const signup_post = async(req, res) => {
     const { name, email, password } = req.body;
     const hashedPassword = await bcrypt.hash(password, 10);
@@ -13,21 +13,22 @@ const signup_post = async(req, res) => {
         name: name,
         email: email,
 
-        password: hashPassword 
+        password: hashedPassword
     }
-  
+    const user = await User.create(newUser)
     console.log(newUser);
-          await newUser.save()
-    res.redirect('/signin'); 
-}
-const signin_post = (req, res) => {
-}
-const get_all_user = (req, res) => {
-}
-const get_user = (req, res) => {
-}
-const delete_user = (req, res) => {
-}
+    await user.save();
+
+    const token = await jwt.sign({ user }, process.env.SCRET_KEY, {
+        expiresIn: '1hr'
+    });
+    res.json({ mesage: "User registration successful", user, token });
+};
+
+const signin_post = (req, res) => {}
+const get_all_user = (req, res) => {}
+const get_user = (req, res) => {}
+const delete_user = (req, res) => {}
 
 module.exports = {
     signin_post,
